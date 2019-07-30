a672b529a54c8f51c071d4beda6d2394f4376d81
Claudia Falk <claudiafalk0@gmail.com>
2019-07-30 13:15:56 -0600

    added array

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import imagesCard from "./components/imagesCard";
+import Wrapper from "./components/Wrapper";
+import images from './images.json.js';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    images
+  }
+
+
+  render(){
+      return(
+    <Wrapper>
+
+      <h1 className='title'>Click on a picture to play!</h1>
+      {this.state.images.map(image => (
+        <imagesCard
+          image={image.image}
+          click = 
+          key = {image.id}
+
+          />
+      ))}
+    </Wrapper>
+      )
+  }
 }
 
 export default App;
+
---
+++ clickygame/src/imagesCard/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function FriendCard(props) {
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <span className="clicked"></span> 
+    </div>
+  );
+}
+
+export default FriendCard;
