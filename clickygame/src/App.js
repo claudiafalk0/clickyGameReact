f2bcd9058d0ea592107f6f598f930bd402d3b6c9
Claudia Falk <claudiafalk0@gmail.com>
2019-07-31 19:07:11 -0600

    added score counter

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -11,13 +11,15 @@ shuffle(images);
 const InitialArr = { images };
 
 class App extends Component {
-  // state = {
-  //   images
-  // };
-  constructor(props) {
-    super(props);
-    this.state = InitialArr
-  }
+  state = {
+    images,
+    count: 0
+  };
+  // constructor(props) {
+  //   super(props);
+  //   this.state = InitialArr
+  //   let count = 0;
+  // }
 
   restart = () => {
     alert(`I'm sorry but you've already chosen that image`);
@@ -27,21 +29,28 @@ class App extends Component {
 
   };
 
+  changeCount = () => {
+    this.setState({count: this.state.count + 1})
+  };
+
   changeClick = (id) => {
 
       let flag = false, clicker = this.state.images.map(
           function (item) {
               if (item.id === id && !item.click) {
                   item.click = true;
-              } else if (item.id === id && item.click === true) {
+                } else if (item.id === id && item.click === true) {
                   flag = true;
-              }
-              return true;
-          });
-      if (flag) {
-          this.restart()
-      } else {
-          this.setState({clicker});
+                }
+                return true;
+              });
+              if (flag) {
+                this.restart()
+                this.setState({count: 0})
+                
+              } else {
+                this.setState({clicker});
+                this.changeCount()
       }
       shuffle(images);
 
@@ -51,6 +60,9 @@ class App extends Component {
     return (
       <Wrapper>
         <h1 className='title'>Click an image to begin</h1>
+        <div>
+          Score: {this.state.count}
+        </div>
         {this.state.images.map(item => (
           <ImagesCard
             id={item.id}
